f4071095fb0912486db85eb82e5366b0ebcc989e
mathieu <mayeux.marignier@gmail.com>
2022-12-04 21:44:00 +0100

    ex5 + rename files

--- ex5/ft_print_comb.js
+++ ex5/ft_print_comb.js
@@ -1,29 +1,12 @@
-const chiffres = Array.from({ length: 10 }, (_, i) => i);
-const result = [];
-
-for (const a of chiffres) {
-  for (const b of chiffres) {
-    for (const c of chiffres) {
-      if (a === b & a === c & b === c) continue;
-
-      const finded = result.find(
-        (search) =>
-          search === a.toString() + b.toString() + c.toString(), 
-          search === a.toString() + c.toString() + b.toString(), 
-          search === b.toString() + a.toString() + c.toString(), 
-          search === b.toString() + c.toString() + a.toString(), 
-          search === c.toString() + a.toString() + b.toString() ||
-          search === c.toString() + b.toString() + a.toString()
-      );
-
-      if (finded) {
-        console.log("degagge", finded, a, b, c);
-        continue;
+function combination() {
+  let array=[]
+  for (let i = 0; i < 10; i++) {
+    for (let j = i + 1; j < 10; j++) {
+      for (let k = j + 1; k < 10; k++) {
+          array+= [i] + [j] + [k] + '. ';
       }
-
-      result.push(a.toString() + b.toString() + c.toString());
     }
   }
+  console.log(array);
 }
-// À test
-console.log(result)
+combination();
